423eeb763b2180853fae7e5c3f7eb7e0a369d0cd
devdivyanshu29 <157698596+devdivyanshu29@users.noreply.github.com>
2024-05-18 05:19:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+}
+)
